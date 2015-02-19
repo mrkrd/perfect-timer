2b18dbbb590272e723f08319134af182c886b0f1
Marek Rudnicki <marekrud@gmail.com>
2015-02-19 21:52:49 +0100

    2x2

--- src/perfect_timer/core.clj
+++ src/perfect_timer/core.clj
@@ -1,5 +1,30 @@
 (ns perfect-timer.core
+  (:use seesaw.core)
   (:gen-class))
+
+(native!)
+
+(def main_frame (frame :title "Perfect Timer"))
+
+
+(-> main_frame pack! show!)
+
+
+(def status_label (label))
+(def input_field (text))
+(def start_button (button :text "Start"))
+(def reset_button (button :text "Reset"))
+
+
+(def grid (grid-panel :rows 2
+                      :columns 2
+                      :items [status_label input_field start_button reset_button]))
+
+
+(config! main_frame :content grid)
+
+
+; XXX clojure timer needed
 
 (defn -main
   "I don't do a whole lot ... yet."
