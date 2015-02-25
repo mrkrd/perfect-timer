94827fd7c34c6854aca3aac18fee3f2c592f15ef
Marek Rudnicki <marekrud@gmail.com>
2015-02-25 23:07:25 +0100

    basic state and timer

--- src/perfect_timer/core.clj
+++ src/perfect_timer/core.clj
@@ -1,36 +1,59 @@
 (ns perfect-timer.core
   (:use seesaw.core)
-  (:use overtone.at-at)
   (:gen-class))
 
 (native!)
 
 
 
-(def status_label (label))
-(def input_field (text))
-(def start_button (button :text "Start"))
-(def reset_button (button :text "Reset"))
+(def status-label (label))
+(def input-field (text :text "3600"))
+(def start-button (button :text "Start"))
+(def reset-button (button :text "Reset"))
 
 
 (def grid (grid-panel :rows 2
                       :columns 2
-                      :items [status_label input_field start_button reset_button]))
+                      :items [status-label input-field start-button reset-button]))
 
 
+(def state (atom {:mode "paused"
+                  :start-time 0
+                  :end-time 0
+                  :passed-time 0}))
+
+
+(defn start-pressed []
+  (swap! state assoc-in [:mode] "paused")
+  (config! start-button :text "Pause")
+  (swap! state assoc-in [:end-time] (read-string (config input-field :text))))
+
+(defn pause-pressed []
+  (swap! state assoc-in [:mode] "started")
+  (config! start-button :text "Start"))
+
+(defn start-or-pause-pressed [e]
+  (if (= (@state :mode) "started")
+    (start-pressed)
+    (pause-pressed)))
+
+(def start-listener (listen start-button :action start-or-pause-pressed))
+; (start-listener)
 
 
 ; XXX clojure timer needed: need to update the GUI every sec or so
 (System/currentTimeMillis)
 
 (defn tick []
-  (config! status_label :text (System/currentTimeMillis) 1000))
+  (config! status-label :text (str @state)) ;(System/currentTimeMillis))
+
+)
 
 
-(def my-pool (mk-pool))
-;(every 1000 #(println "I am cool!") my-pool)
-(every 1000 tick my-pool)
-(stop-and-reset-pool! my-pool)
+(def t (timer (fn [e] (tick)) :delay 1000))
+
+(.stop t)
+
 
 
 (defn -main [& args]
