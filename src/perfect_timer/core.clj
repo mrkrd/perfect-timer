48546dee210c13fcb21aa3e9afc5e50013405b51
Marek Rudnicki <marekrud@gmail.com>
2015-03-02 00:24:10 +0100

    basic operation

--- src/perfect_timer/core.clj
+++ src/perfect_timer/core.clj
@@ -4,55 +4,90 @@
 
 (native!)
 
-
+;(use 'seesaw.dev)
+;(seesaw.dev/debug!)
 
 (def status-label (label))
 (def input-field (text :text "3600"))
 (def start-button (button :text "Start"))
 (def reset-button (button :text "Reset"))
+(def main-progress (progress-bar :max 3600000))
 
 
-(def grid (grid-panel :rows 2
-                      :columns 2
-                      :items [status-label input-field start-button reset-button]))
+(def grid (grid-panel :columns 2
+                      :items [status-label input-field
+                              start-button reset-button
+                              main-progress]))
 
 
-(def state (atom {:mode "paused"
-                  :start-time 0
-                  :end-time 0
-                  :passed-time 0}))
+(def state (atom {:starts []
+                  :pauses []
+                  :tmax 0}))
 
 
 (defn start-pressed []
-  (swap! state assoc-in [:mode] "paused")
+  ;; UI changes
   (config! start-button :text "Pause")
-  (swap! state assoc-in [:end-time] (read-string (config input-field :text))))
+  (config! input-field :editable? false)
+
+  ;; State changes
+  (if (empty? (@state :starts))            ; First press?
+    (do
+      (swap! state assoc-in [:tmax] (* (read-string (config input-field :text)) 1000))
+      (config! main-progress :max (@state :tmax))))
+  (swap! state update-in [:starts] conj (System/currentTimeMillis)))
+
+
 
 (defn pause-pressed []
-  (swap! state assoc-in [:mode] "started")
-  (config! start-button :text "Start"))
+  ;; UI changes
+  (config! start-button :text "Start")
+
+  ;; State changes
+  (swap! state update-in [:pauses] conj (System/currentTimeMillis)))
+
 
 (defn start-or-pause-pressed [e]
-  (if (= (@state :mode) "started")
-    (start-pressed)
-    (pause-pressed)))
+  (if (> (count (@state :starts)) (count (@state :pauses)))
+    (pause-pressed)
+    (start-pressed)))
 
 (def start-listener (listen start-button :action start-or-pause-pressed))
 ; (start-listener)
 
 
-; XXX clojure timer needed: need to update the GUI every sec or so
-(System/currentTimeMillis)
+(defn reset-pressed [e]
+  ;; UI changes
+  (config! input-field :editable? true)
+  (config! start-button :text "Start")
+  (config! main-progress :value 0)
+
+  ;; State changes
+  (swap! state assoc-in [:starts] [])
+  (swap! state assoc-in [:pauses] []))
+
+
+(def reset-listener (listen reset-button :action reset-pressed))
+; (reset-listener)
+
 
 (defn tick []
-  (config! status-label :text (str @state)) ;(System/currentTimeMillis))
+  (let [starts (@state :starts)
+        pauses (conj (@state :pauses) (System/currentTimeMillis))
+        time-passed (reduce + (map - pauses starts))]
 
-)
+    (config! status-label :text (format "%.1f" (/ time-passed 1000.0)))
+    (config! main-progress :value time-passed)
+
+    (if (> time-passed (@state :tmax))
+      (do
+        (alert "Time's up!")
+        (reset-pressed nil)))))
 
 
-(def t (timer (fn [e] (tick)) :delay 1000))
 
-(.stop t)
+(def main-timer (timer (fn [e] (tick)) :delay 100))
+; (.stop main-timer)
 
 
 
@@ -61,7 +96,7 @@
    (->
     (frame
      :title "Perfect Timer"
-     ;; :on-close :exit
+     :on-close :exit
      :content grid)
     pack!
     show!)))
